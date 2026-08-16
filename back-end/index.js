1845c26d19bd7119d3fe255c610885b7c7fb736c
Priyanka <dhankumaripriyanka@gmail.com>
2026-08-16 13:07:50 +0530

    add some APIs

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,39 @@
-const express= require('express');
+const express = require('express');
 const cors = require('cors');
 require('./db/config');
-const User= require('./db/User');
+const User = require('./db/User');
+const Product = require('./db/Product');
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.post("/register",async (req,resp)=>{
-    let user= new User(req.body);
-    let result= await user.save();
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password
     resp.send(result);
+})
+
+app.post("/login", async (req, resp) => {
+    console.log(req.body)
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user);
+        } else {
+            resp.send({ result: 'no user found' });
+        }
+    } else {
+        resp.send({ result: 'no user found' });
+    }
+
+    app.post("/add-product", async (req, resp) => {
+        let product = new Product(req.body);
+        let result = await product.save();
+        resp.send(result);
+    })
+
 })
 app.listen(5000);
