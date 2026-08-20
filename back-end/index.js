2fe865803fd538730b6a3cc699c10944b2153b58
Priyanka <dhankumaripriyanka@gmail.com>
2026-08-20 09:27:46 +0530

    add delete and update product api.

--- back-end/index.js
+++ back-end/index.js
@@ -36,18 +36,37 @@ app.post("/add-product", async (req, resp) => {
     resp.send(result);
 })
 
-app.get("/products", async (req, resp)=>{
-let Products =await Product.find()
-if(Products.length>0){
-    resp.send(Products)
-}else{
-    resp.send({result:"No Products found"})
-}
+app.get("/products", async (req, resp) => {
+    let Products = await Product.find()
+    if (Products.length > 0) {
+        resp.send(Products)
+    } else {
+        resp.send({ result: "No Products found" })
+    }
 });
 
-app.delete("/product/:id", async (req, resp)=>{
-    const result = await Product.deleteOne({_id:req.params.id})
+app.delete("/product/:id", async (req, resp) => {
+    const result = await Product.deleteOne({ _id: req.params.id })
     resp.send(result);
+});
+
+app.get("/product/:id", async (req, resp) => {
+    const result = await Product.findOne({ _id: req.params.id })
+    if (result) {
+        resp.send(result)
+    } else {
+        resp.send({ result: "no record found." })
+    }
+})
+
+app.put("/product/:id", async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        {
+            $set : req.body
+        }
+    )
+    resp.send(result)
 })
 
 app.listen(5000);
